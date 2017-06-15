dc44208a01fef583ae93a2c4ab6b63a26d98a785
Jongkyu Kim <xenigmax@gmail.com>
2017-06-15 12:28:31 +0200

    disable automatically when using single evidence type

--- src/breakpoint.cpp
+++ src/breakpoint.cpp
@@ -816,6 +816,12 @@ bool BreakpointManager::filterByEvidenceSumAndVote(void)
         this->getOptionManager()->setMinVote(minVote);
     }
 
+    if (minVote == 1)
+    {
+        printTimeMessage("Voting based resecuing is disabled automatically.");
+        minVote = MaxValue<int32_t>::VALUE;
+    }
+
     TBreakpointSet* candidateSet = this->getMergedBreakpoint()->getCandidateSet();
     auto itBreakpoint = candidateSet->begin();
     unsigned filteredBreakpointCount = 0;
